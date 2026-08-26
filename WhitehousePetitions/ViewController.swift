bcdd1678c36034fd9bef33e7ddb8edff83392c19
merve-bulut <mervebult6@gmail.com>
2026-08-26 21:36:14 +0300

    Add error handling

--- WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/ViewController.swift
@@ -24,10 +24,19 @@ class ViewController: UITableViewController {
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        
+        showError()
     }
 
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
     func parse(json: Data) {
         let decoder = JSONDecoder()
         
